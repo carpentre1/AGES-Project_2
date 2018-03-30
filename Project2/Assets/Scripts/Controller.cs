4cdf44366648bf3290e3ff60316fdeafd54557e3
carpentre1 <jlowell.carpenter@gmail.com>
2018-03-29 21:35:23 -0500

    Golf ball movement
    Plug in controller to test golf ball: left analog stick to adjust angle/intensity, A button to putt the ball.
    
    The ball can't seem to move in the right / upper right direction?

---
+++ Project2/Assets/Scripts/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+    public GolfBall ballScript;
+    public GameObject golfBall;
+
+    float hInput = 0;
+    float vInput = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetAxis("Horizontal") > 0 || Input.GetAxis("Horizontal") < 0)
+        {
+            hInput = -Input.GetAxis("Horizontal");
+        }
+        if (Input.GetAxis("Vertical") > 0 || Input.GetAxis("Vertical") < 0)
+        {
+            vInput = Input.GetAxis("Vertical");
+        }
+        ballScript.RotateBall(hInput, vInput);
+
+        if (Input.GetButtonDown("P1_A"))//if they press the A button
+        {
+            float intensity = Mathf.Max(hInput, vInput);//the force to putt the ball, based on how tilted the analog stick was
+            ballScript.Putt(intensity);
+        }
+    }
+}
---
+++ Project2/Assets/Scripts/GolfBall.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GolfBall : MonoBehaviour {
+
+    Rigidbody rb;
+    public GameObject ballDirection;
+
+    public float xAxis;
+    public float zAxis;
+    Vector3 originalPos;
+
+    public float extraThrust = 200;//the amount of extra power for tuning how far the ball can go
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        originalPos = ballDirection.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        originalPos = transform.position;//keep the ball direction indicator in the same spot as the ball itself
+	}
+
+    public void RotateBall(float vInput, float hInput)
+    {
+        ballDirection.transform.position = new Vector3(originalPos.x - vInput, originalPos.y, originalPos.z - hInput);//move the 'ball direction indicator' in the direction of the analog; this works perfectly
+
+    }
+    public void Putt(float thrust)
+    {
+        rb.velocity = new Vector3(0, 0, 0);//reset the ball's velocity before we putt it again
+        transform.LookAt(ballDirection.transform);//make the ball stare in the direction of the 'ball direction indicator'. this also works as intended
+        thrust *= extraThrust;//thrust is 0 to 1, extraThrust is a public variable used for tuning how far the ball should go
+        rb.AddForce(transform.forward * thrust);//send the ball in its forwards direction. has weird issue where it can't send in the upper right and right directions? all other directions work as intended
+    }
+}
