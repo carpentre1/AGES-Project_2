6b9b89aa2f3f335887c3f3830be43868d07eda84
carpentre1 <jlowell.carpenter@gmail.com>
2018-04-09 20:25:35 -0500

    Added probuilder, created golf hole

---
+++ Project2/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+    public float xOffset;
+    public float yOffset;
+    public float zOffset;
+
+    public Transform golfBall;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(golfBall.position.x + xOffset, golfBall.position.y + yOffset, golfBall.position.z + zOffset);
+    }
+}
